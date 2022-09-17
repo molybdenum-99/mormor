601c5b6fd9a75cc6666f5fc284eb63b13149321b
glaszig <mail@glasz.org>
2022-09-17 00:22:00 -0300

    add german to test script
    and patch around encoding name issue:
      code converter not found (UTF-8 to utf8) (Encoding::ConverterNotFoundError)

--- test/words.rb
+++ test/words.rb
@@ -6,9 +6,13 @@ require_relative '../lib/mormor'
   ukrainian: %w[чоловічим солов'їна п'яним],
   swedish: %w[andra omstritt kämpa],
   portuguese: %w[cachorrinho],
-  french: %w[parlions]
+  french: %w[parlions],
+  german: %w[abbehalten]
 }.each do |lang, words|
   dic = MorMor::Dictionary.new(File.expand_path(lang.to_s, __dir__))
+  if "utf8" == dic.instance_variable_get(:@encoding)
+    dic.instance_variable_set :@encoding, Encoding::UTF_8
+  end
   puts "#{lang}\n" + "-" * lang.length
   words.each do |word|
     found = dic.lookup(word)
